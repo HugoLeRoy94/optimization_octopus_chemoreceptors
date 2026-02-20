c33b8d96e059ffcefb38e4f8b1b51386ba35431a
HugoLeRoy94 <leroyhugo@yahoo.fr>
2026-02-20 09:15:59 +0100

    adapt __init__ and couple of notebook test

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,19 @@
 # core/__init__.py
 
-from .environment import LigandEnvironment
-from .physics import MWCReceptorLayer
+from .environment import (
+    LigandEnvironment, 
+    ConcentrationModel, 
+    LogNormalConcentration, 
+    NormalConcentration
+)
+from .physics import Receptor
 
-# This allows to do: from core import LigandEnvironment, MWCReceptorLayer
-__all__ = ["LigandEnvironment", "MWCReceptorLayer"]
+# Exposing these allows for clean imports like:
+# from core import MWCReceptorLayer, NormalConcentration
+__all__ = [
+    "LigandEnvironment", 
+    "Receptor", 
+    "ConcentrationModel", 
+    "LogNormalConcentration", 
+    "NormalConcentration"
+]
